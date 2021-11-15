a2fab356fb5d71a219373029a0a2740142eb75c3
Meder <meder@kamalov.net>
2021-11-15 10:53:56 +0100

    Add datadog event example

---
+++ fal_scripts/send_datadog_event.py
@@ -0,0 +1,30 @@
+"""Send event with model information to Datadog."""
+from datadog_api_client.v1 import ApiClient, ApiException, Configuration
+from datadog_api_client.v1.api import events_api
+from datadog_api_client.v1.models import EventCreateRequest
+import os
+import time
+
+current_time = time.time()
+
+configuration = Configuration()
+configuration.api_key['apiKeyAuth'] = os.getenv("DD_API_KEY")
+configuration.api_key['appKeyAuth'] = os.getenv("DD_APP_KEY")
+tags = ["fal"]
+df = ref(context.current_model.name)
+text = df.info()
+
+with ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    events_api_instance = events_api.EventsApi(api_client)
+    event_body = EventCreateRequest(
+        tags=tags,
+        aggregation_key="fal",
+        title="fal - event",
+        text=text,
+        date_happened=int(current_time)
+    )
+    try:
+        events_api_instance.create_event(event_body)
+    except ApiException as e:
+        assert e.response["error"]
--- models/zendesk_ticket_metrics.sql
+++ models/zendesk_ticket_metrics.sql
@@ -1,3 +1,6 @@
+-- source table contains zendesk ticket data
+-- more info: https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_metrics/
+
 SELECT agent_wait_time_in_minutes.business as y,
        CAST(DATE(created_at) as DATE) as ds
 FROM `learning-project-305919.dbt_meder.zendesk_ticket_data`
