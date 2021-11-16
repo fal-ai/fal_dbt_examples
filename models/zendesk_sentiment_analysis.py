a6026fae1e725272fda9e2360a86b7448621d697
Meder <meder@kamalov.net>
2021-11-16 19:28:55 +0100

    add dbt seed

--- models/zendesk_sentiment_analysis.py
+++ models/zendesk_sentiment_analysis.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 
-# ticket_descriptions = list(ref("stg_zendesk_ticket_data").description)
-# classifier = pipeline("sentiment-analysis")
-# print(classifier(ticket_descriptions))
-## todo write the analysis back to a table..
+ticket_descriptions = list(ref("stg_zendesk_ticket_data").description)
+classifier = pipeline("sentiment-analysis")
+print(classifier(ticket_descriptions))
+# todo write the analysis back to a table..
--- models/zendesk_ticket_metrics.sql
+++ models/zendesk_ticket_metrics.sql
@@ -3,4 +3,4 @@
 
 SELECT agent_wait_time_in_minutes.business as y,
        CAST(DATE(created_at) as DATE) as ds
-FROM `learning-project-305919.dbt_meder.zendesk_ticket_data`
+FROM `learning-project-305919.dbt_meder.zendesk_ticket_metric_data`
