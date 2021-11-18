686e6e29a0095f0efa4d6a21c9c1c9861246b51a
Meder <meder@kamalov.net>
2021-11-18 12:25:12 +0100

    Update datadog example

--- fal_scripts/send_datadog_event.py
+++ fal_scripts/send_datadog_event.py
@@ -11,24 +11,27 @@ current_time = time.time()
 configuration = Configuration()
 configuration.api_key['apiKeyAuth'] = os.getenv("DD_API_KEY")
 configuration.api_key['appKeyAuth'] = os.getenv("DD_APP_KEY")
-tags = ["fal"]
 
 df = ref(context.current_model.name)
 
 buf = io.StringIO()
 df.info(buf=buf)
+
 text = buf.getvalue()
+tags = ["fal"]
+
+event_body = EventCreateRequest(
+    tags=tags,
+    aggregation_key="fal",
+    title="fal - event",
+    text=text,
+    date_happened=int(current_time)
+)
+
 
 with ApiClient(configuration) as api_client:
     # Create an instance of the API class
     events_api_instance = events_api.EventsApi(api_client)
-    event_body = EventCreateRequest(
-        tags=tags,
-        aggregation_key="fal",
-        title="fal - event",
-        text=text,
-        date_happened=int(current_time)
-    )
     try:
         events_api_instance.create_event(event_body)
     except ApiException as e:
