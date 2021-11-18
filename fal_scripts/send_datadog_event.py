11ba672c01147e2a3bbfdf5d21ab91d8aac41654
Meder <meder@kamalov.net>
2021-11-18 12:09:57 +0100

    Update datadog example

--- fal_scripts/send_datadog_event.py
+++ fal_scripts/send_datadog_event.py
@@ -4,6 +4,7 @@ from datadog_api_client.v1.api import events_api
 from datadog_api_client.v1.models import EventCreateRequest
 import os
 import time
+import io
 
 current_time = time.time()
 
@@ -11,8 +12,12 @@ configuration = Configuration()
 configuration.api_key['apiKeyAuth'] = os.getenv("DD_API_KEY")
 configuration.api_key['appKeyAuth'] = os.getenv("DD_APP_KEY")
 tags = ["fal"]
+
 df = ref(context.current_model.name)
-text = df.info()
+
+buf = io.StringIO()
+df.info(buf=buf)
+text = buf.getvalue()
 
 with ApiClient(configuration) as api_client:
     # Create an instance of the API class
