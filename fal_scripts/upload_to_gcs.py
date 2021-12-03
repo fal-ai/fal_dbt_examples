5e1ad8748078c211d574cd1603b6d3fae6df739a
turbo1912 <yurtseven.gorkem@gmail.com>
2021-12-03 14:58:03 -0800

    better example

--- fal_scripts/upload_to_gcs.py
+++ fal_scripts/upload_to_gcs.py
@@ -2,10 +2,16 @@ import os
 from google.cloud import storage
 
 bucket_name = "fal_example_dbt_artifacts_bucket"
-destination_blob_name = "manifest.json"
-source_file_name = os.path.join(context.config.target_path, "manifest.json")
+manifest_destination_blob_name = "manifest.json"
+run_results_destination_blob_name = "run_results.json"
+
+manifest_source_file_name = os.path.join(context.config.target_path, "manifest.json")
+run_results_source_file_name = os.path.join(context.config.target_path, "run_results.json")
 
 storage_client = storage.Client()
 bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(destination_blob_name)
-blob.upload_from_filename(source_file_name)
+manifest_blob = bucket.blob(manifest_destination_blob_name)
+run_results_blob = bucket.blob(run_results_destination_blob_name)
+
+manifest_blob.upload_from_filename(manifest_source_file_name)
+run_results_blob.upload_from_filename(run_results_source_file_name)
